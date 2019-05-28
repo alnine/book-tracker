d3db99bacdaa142bee890956ab6bc81f28a6f6ae
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-05-28 23:26:07 +0300

    Prepared templates of layout for components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <form className="search-form search-form--centered">
+      <form className="search-form search-form--on-top">
         <input
           className="search-form__field"
           type="text"
@@ -13,6 +13,67 @@ function App() {
         />
         <button className="search-form__btn">Search</button>
       </form>
+      <h2 className="request">Searching Request</h2>
+      <ul className="book-list">
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <img
+              src="https://via.placeholder.com/205x265"
+              alt=""
+              className="book__cover"
+              width="205"
+              height="265"
+            />
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <div className="book__stub">
+              <span className="book__stub-title">Book #</span>
+            </div>
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <div className="book__stub">
+              <span className="book__stub-title">Book #</span>
+            </div>
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <div className="book__stub">
+              <span className="book__stub-title">Book #</span>
+            </div>
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <div className="book__stub">
+              <span className="book__stub-title">Book #</span>
+            </div>
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+        <li className="book-list__item">
+          <a href="#" className="book">
+            <div className="book__stub">
+              <span className="book__stub-title">Book #</span>
+            </div>
+            <h4 className="book__author">Author</h4>
+            <h3 className="book__title">Book Title</h3>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
